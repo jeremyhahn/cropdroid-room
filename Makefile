bf9da8f14e2c9b79ebffc3e2302b2d76b220f0bf
Jeremy Hahn <mail@jeremyhahn.com>
2019-03-10 18:30:12 -0400

    optimize memory. expose remaining analog and digital pins as switches

--- Makefile
+++ Makefile
@@ -9,5 +9,8 @@ default: flash
 flash:
 	avrdude -c usbasp -p m328p -u -U flash:w:build/Nano/harvest-room.hex
 
+flash-nano:
+	avrdude -v -patmega328p -carduino -P/dev/ttyUSB0 -b115200 -D -U flash:w:build/Nano/harvest-room.hex
+
 flash-mega:
 	avrdude -c usbasp -p m2560 -u -U flash:w:build/Mega/harvest-room.hex
--- main.cpp
+++ main.cpp
@@ -9,13 +9,19 @@
 #define BUFSIZE 100
 
 #define ONE_WIRE_BUS 2
-#define DHT_ROOM_PIN 5
-#define DEHU_PIN 6
-#define FAN_PIN 7
+#define DHT_ROOM_PIN 3
 #define CO2_PIN A0
 #define WATER0_PIN A1
 #define WATER1_PIN A2
 #define PHOTO_PIN A3
+
+extern int  __bss_end;
+extern int  *__brkval;
+
+const int channel_size = 10;
+const int valid_channels[channel_size] = {
+	4, 5, 6, 7, 8, 9, A4, A5, A6, A7
+};
 
 const char string_initializing[] PROGMEM = "Initializing...";
 const char string_dhcp_failed[] PROGMEM = "DHCP Failed";
@@ -24,10 +30,21 @@ const char string_http_404[] PROGMEM = "HTTP/1.1 404 Not Found";
 const char string_http_500[] PROGMEM = "HTTP/1.1 500 Internal Server Error";
 const char string_http_xpowered_by[] PROGMEM = "X-Powered-By: Harvest Room Controller v1.0";
 const char string_rest_address[] PROGMEM = "REST service listening on: ";
-const char string_dehu_start[] PROGMEM = "Starting dehumidifier...";
-const char string_dehu_stop[] PROGMEM = "Stopping dehumidifier...";
-const char string_fan_start[] PROGMEM = "Starting fan...";
-const char string_fan_stop[] PROGMEM = "Stopping fan...";
+const char string_switch_on[] PROGMEM = "Switching on";
+const char string_switch_off[] PROGMEM = "Switching off";
+const char string_json_key_mem[] PROGMEM = "\"mem\":";
+const char string_json_key_tempF[] PROGMEM = ",\"tempF\":";
+const char string_json_key_tempC[] PROGMEM = ",\"tempC\":";
+const char string_json_key_humidity[] PROGMEM = ",\"humidity\":";
+const char string_json_key_heatIndex[] PROGMEM = ",\"heatIndex\":";
+const char string_json_key_vpd[] PROGMEM = ",\"vpd\":";
+const char string_json_key_pod0[] PROGMEM = ",\"pod0\":";
+const char string_json_key_pod1[] PROGMEM = ",\"pod1\":";
+const char string_json_key_co2[] PROGMEM = ",\"co2\":";
+const char string_json_key_water0[] PROGMEM = ",\"water0\":";
+const char string_json_key_water1[] PROGMEM = ",\"water1\":";
+const char string_json_key_photo[] PROGMEM = ",\"photo\":";
+const char string_json_key_channels[] PROGMEM = ",\"channels\":{";
 const char * const string_table[] PROGMEM = {
   string_initializing,
   string_dhcp_failed,
@@ -36,10 +53,21 @@ const char * const string_table[] PROGMEM = {
   string_http_500,
   string_http_xpowered_by,
   string_rest_address,
-  string_dehu_start,
-  string_dehu_stop,
-  string_fan_start,
-  string_fan_stop
+  string_switch_on,
+  string_switch_off,
+  string_json_key_mem,
+  string_json_key_tempF,
+  string_json_key_tempC,
+  string_json_key_humidity,
+  string_json_key_heatIndex,
+  string_json_key_vpd,
+  string_json_key_pod0,
+  string_json_key_pod1,
+  string_json_key_co2,
+  string_json_key_water0,
+  string_json_key_water1,
+  string_json_key_photo,
+  string_json_key_channels
 };
 int idx_initializing = 0,
     idx_dhcp_failed = 1,
@@ -48,11 +76,23 @@ int idx_initializing = 0,
 	idx_http_500 = 4,
 	idx_http_xpowered_by = 5,
 	idx_rest_address = 6,
-	idx_dehu_start = 7,
-	idx_dehu_stop = 8,
-	idx_fan_start = 9,
-	idx_fan_stop = 10;
+	idx_switch_on = 7,
+	idx_switch_off = 8,
+	idx_json_key_mem = 9,
+	idx_json_key_tempF = 10,
+	idx_json_key_tempC = 11,
+	idx_json_key_humidity = 12,
+	idx_json_key_heatIndex = 13,
+	idx_json_key_vpd = 14,
+	idx_json_key_pod0 = 15,
+	idx_json_key_pod1 = 16,
+	idx_json_key_co2 = 17,
+	idx_json_key_water0 = 18,
+	idx_json_key_water1 = 19,
+	idx_json_key_photo = 20,
+	idx_json_key_channels = 21;
 char string_buffer[50];
+char float_buffer[10];
 
 DHT roomDHT(DHT_ROOM_PIN, DHT22);
 OneWire oneWire(ONE_WIRE_BUS);
@@ -71,13 +111,12 @@ void co2ppm();
 void readWaterLevels();
 void handleWebRequest();
 void send404();
-void startDehu();
-void stopDehu();
-void startFan();
-void stopFan();
+void switchOn(int pin);
+void switchOff(int pin);
+int availableMemory();
 void(* resetFunc) (void) = 0;
 
-byte mac[] = { 0x04, 0x02, 0x00, 0x00, 0x00, 0x01 };
+byte mac[] = { 0x04, 0x02, 0x00, 0x00, 0x02, 0x01 };
 IPAddress ip(192,168,0,200);
 
 EthernetServer httpServer(80);
@@ -93,13 +132,14 @@ int main(void) {
   return 0;
 }
 
-
-
 void setup(void) {
 
   analogReference(DEFAULT);
-  pinMode(DEHU_PIN, OUTPUT);
-  pinMode(FAN_PIN, OUTPUT);
+
+  for(int i=0; i<channel_size; i++) {
+	  pinMode(i, OUTPUT);
+	  digitalWrite(i, LOW);
+  }
 
 #if DEBUG
   Serial.begin(115200);
@@ -109,8 +149,10 @@ void setup(void) {
 #endif
 
   if(Ethernet.begin(mac) == 0) {
-    strcpy_P(string_buffer, (char*)pgm_read_word(&(string_table[idx_dhcp_failed])));
-    Serial.println(string_buffer);
+	#if DEBUG
+      strcpy_P(string_buffer, (char*)pgm_read_word(&(string_table[idx_dhcp_failed])));
+      Serial.println(string_buffer);
+	#endif
     Ethernet.begin(mac, ip);
   }
 
@@ -139,7 +181,7 @@ void loop() {
 
   digitalWrite(LED_BUILTIN, LOW);
 
-  delay(2000);
+  delay(2000); // DHT22 requires 2 seconds between polls
 }
 
 void calculateVPD(float temp, float rh) {
@@ -150,7 +192,6 @@ void calculateVPD(float temp, float rh) {
   float es = 0.6107 * exp(7.5 * temp / (temp + 237.3));
   float ea = rh / 100 * es;
   VPD = ea - es;
-
 
   //float svp = 610.78 * exp(temp / (temp + 238.3) * 17.2694);
   //VPD = svp * (1 - rh / 100);
@@ -203,25 +244,21 @@ void readRoomTempHumidity() {
 void co2ppm() {
 
   int sensorValue = analogRead(CO2_PIN);
-
   float voltage = sensorValue * (5000 / 1024.0);
-  if(voltage == 0)
-  {
+
+  if(voltage == 0) {
 	#if DEBUG
       Serial.println("CO2 Fault");
 	#endif
   }
-  else if(voltage < 400)
-  {
+  else if(voltage < 400) {
 	#if DEBUG
       Serial.println("Preheating CO2 sensor");
 	#endif
   }
-  else
-  {
-    int voltage_diference = voltage-400;
-    float concentration = voltage_diference*50.0/16.0;
-
+  else {
+    int voltage_diference = voltage - 400;
+    float concentration = voltage_diference *50.0 / 16.0;
 	#if DEBUG
 	  Serial.print("co2 voltage:");
 	  Serial.print(voltage);
@@ -239,46 +276,44 @@ void readWaterLevels() {
   waterLevel1 = analogRead(WATER1_PIN) * 100 / 1024;
 
 #if DEBUG
-  Serial.print("Water0: " );
-  Serial.print(waterLevel0);
-  Serial.println("%");
 
-  Serial.print("Water1: " );
-  Serial.print(waterLevel1);
-  Serial.println("%");
+  itoa(waterLevel0, float_buffer, 10);
+  strcpy(string_buffer, "Water0: ");
+  strcat(string_buffer, float_buffer);
+  strcat(string_buffer, "%");
+  Serial.println(string_buffer);
+
+  itoa(waterLevel1, float_buffer, 10);
+  strcpy(string_buffer, "Water1: ");
+  strcat(string_buffer, float_buffer);
+  strcat(string_buffer, "%");
+  Serial.println(string_buffer);
+
 #endif
 }
 
-void startDehu() {
+void switchOn(int pin) {
 #if DEBUG
-  strcpy_P(string_buffer, (char*)pgm_read_word(&(string_table[idx_dehu_start])));
+  char sPin[3];
+  itoa(pin, sPin, 10);
+  strcpy_P(string_buffer, (char*)pgm_read_word(&(string_table[idx_switch_on])));
+  strcat(string_buffer, " pin ");
+  strcat(string_buffer, sPin);
   Serial.println(string_buffer);
 #endif
-  digitalWrite(DEHU_PIN, HIGH);
+  digitalWrite(pin, HIGH);
 }
 
-void stopDehu() {
+void switchOff(int pin) {
 #if DEBUG
-  strcpy_P(string_buffer, (char*)pgm_read_word(&(string_table[idx_dehu_stop])));
+  char sPin[3];
+  itoa(pin, sPin, 10);
+  strcpy_P(string_buffer, (char*)pgm_read_word(&(string_table[idx_switch_off])));
+  strcat(string_buffer, " pin ");
+  strcat(string_buffer, sPin);
   Serial.println(string_buffer);
 #endif
-  digitalWrite(DEHU_PIN, LOW);
-}
-
-void startFan() {
-#if DEBUG
-  strcpy_P(string_buffer, (char*)pgm_read_word(&(string_table[idx_fan_start])));
-  Serial.println(string_buffer);
-#endif
-  digitalWrite(FAN_PIN, HIGH);
-}
-
-void stopFan() {
-#if DEBUG
-  strcpy_P(string_buffer, (char*)pgm_read_word(&(string_table[idx_fan_stop])));
-  Serial.println(string_buffer);
-#endif
-  digitalWrite(FAN_PIN, LOW);
+  digitalWrite(pin, LOW);
 }
 
 void handleWebRequest() {
@@ -287,7 +322,11 @@ void handleWebRequest() {
 
 	char clientline[BUFSIZE];
 	int index = 0;
+
 	bool reset = false;
+	char json[255];
+	char sPin[2];
+	char state[2];
 
 	if (httpClient) {
 
@@ -315,11 +354,16 @@ void handleWebRequest() {
 				char *param1 = strtok(NULL, "/");
 				char *param2 = strtok(NULL, "/");
 
-				//Serial.print("Resource: ");
-				//Serial.println(resource);
+				#if DEBUG
+				  Serial.print("Resource: ");
+				  Serial.println(resource);
 
-				char json[255];
-				char float_buffer[7];
+				  Serial.print("Parm1: ");
+				  Serial.println(param1);
+
+				  Serial.print("Param2: ");
+				  Serial.println(param2);
+				#endif
 
 				int address = atoi(param1);
 
@@ -328,92 +372,116 @@ void handleWebRequest() {
 
 					strcpy(json, "{");
 
-					  strcat(json, "\"tempF\":");
+					  strcpy_P(string_buffer, (char*)pgm_read_word(&(string_table[idx_json_key_mem])));
+					  strcat(json, string_buffer);
+					  itoa(availableMemory(), float_buffer, 10);
+					  strcat(json, float_buffer);
+
+					  strcpy_P(string_buffer, (char*)pgm_read_word(&(string_table[idx_json_key_tempF])));
+					  strcat(json, string_buffer);
 					  dtostrf(roomTempF, 4, 2, float_buffer);
 					  strcat(json, float_buffer);
-					  strcat(json, ", ");
 
-					  strcat(json, "\"tempC\":");
+					  strcpy_P(string_buffer, (char*)pgm_read_word(&(string_table[idx_json_key_tempC])));
+					  strcat(json, string_buffer);
                       dtostrf(roomTempC, 4, 2, float_buffer);
                       strcat(json, float_buffer);
-                      strcat(json, ", ");
 
-                      strcat(json, "\"humidity\":");
+                      strcpy_P(string_buffer, (char*)pgm_read_word(&(string_table[idx_json_key_humidity])));
+					  strcat(json, string_buffer);
                       dtostrf(roomHumidity, 4, 2, float_buffer);
 					  strcat(json, float_buffer);
-					  strcat(json, ", ");
 
-					  strcat(json, "\"heatIndex\":");
+					  strcpy_P(string_buffer, (char*)pgm_read_word(&(string_table[idx_json_key_heatIndex])));
+					  strcat(json, string_buffer);
 					  dtostrf(roomHeatIndex, 4, 2, float_buffer);
 					  strcat(json, float_buffer);
-					  strcat(json, ", ");
 
-					  strcat(json, "\"vpd\":");
+					  strcpy_P(string_buffer, (char*)pgm_read_word(&(string_table[idx_json_key_vpd])));
+					  strcat(json, string_buffer);
 					  dtostrf(VPD, 4, 2, float_buffer);
 					  strcat(json, float_buffer);
-					  strcat(json, ", ");
 
-					  strcat(json, "\"pod0\":");
+					  strcpy_P(string_buffer, (char*)pgm_read_word(&(string_table[idx_json_key_pod0])));
+					  strcat(json, string_buffer);
 					  dtostrf(pod0Temp, 4, 2, float_buffer);
 					  strcat(json, float_buffer);
-					  strcat(json, ", ");
 
-					  strcat(json, "\"pod1\":");
-					  dtostrf(roomHeatIndex, 4, 2, float_buffer);
+					  strcpy_P(string_buffer, (char*)pgm_read_word(&(string_table[idx_json_key_pod1])));
+					  strcat(json, string_buffer);
+					  dtostrf(pod1Temp, 4, 2, float_buffer);
 					  strcat(json, float_buffer);
-					  strcat(json, ", ");
 
-					  strcat(json, "\"co2\":");
+					  strcpy_P(string_buffer, (char*)pgm_read_word(&(string_table[idx_json_key_co2])));
+					  strcat(json, string_buffer);
 					  dtostrf(CO2PPM, 4, 2, float_buffer);
 					  strcat(json, float_buffer);
-					  strcat(json, ", ");
 
-					  strcat(json, "\"water0\":");
+					  strcpy_P(string_buffer, (char*)pgm_read_word(&(string_table[idx_json_key_water0])));
+					  strcat(json, string_buffer);
 					  itoa(waterLevel0, float_buffer, 10);
 					  strcat(json, float_buffer);
-					  strcat(json, ", ");
 
-					  strcat(json, "\"water1\":");
+					  strcpy_P(string_buffer, (char*)pgm_read_word(&(string_table[idx_json_key_water1])));
+					  strcat(json, string_buffer);
 					  itoa(waterLevel1, float_buffer, 10);
 					  strcat(json, float_buffer);
-					  strcat(json, ", ");
 
-					  strcat(json, "\"photo\":");
+					  strcpy_P(string_buffer, (char*)pgm_read_word(&(string_table[idx_json_key_photo])));
+					  strcat(json, string_buffer);
 					  itoa(photo, float_buffer, 10);
 					  strcat(json, float_buffer);
 
+					  strcpy_P(string_buffer, (char*)pgm_read_word(&(string_table[idx_json_key_channels])));
+					  strcat(json, string_buffer);
+					    for(int i=0; i<channel_size; i++) {
+						  itoa(valid_channels[i], sPin, 10);
+						  itoa(digitalRead(valid_channels[i]), state, 10);
+						  strcat(json, "\"c");
+						  strcat(json, sPin);
+						  strcat(json, "\":");
+						  strcat(json, state);
+						  if(i + 1 < channel_size) {
+							  strcat(json, ",");
+						  }
+					    }
+					  strcat(json, "}");
+
 					strcat(json, "}");
 				}
-				// /dehu/?     1 = on, else off
-				else if (strncmp(resource, "dehu", 4) == 0) {
-					#if DEBUG
-						Serial.print("Dehu: ");
-						Serial.println(param1);
-					#endif
-					if(strncmp(param1, "1", 1) == 0) {
-						startDehu();
-						strcpy(json, "{\"dehu\":true}");
+
+				// /switch/?     1 = on, else off
+				else if (strncmp(resource, "switch", 7) == 0) {
+
+					int pin = atoi(param1);
+					int position = atoi(param2);
+
+					bool valid = false;
+					for(int i=0; i<channel_size; i++) {
+						if(pin == valid_channels[i]) {
+							valid = true;
+							break;
+						}
+					}
+					if(valid) {
+						strcpy(json, "{\"pin\":");
+						strcat(json, param1);
+						strcat(json, ", \"position: \"");
+						strcat(json, param2);
+						strcat(json, "}");
+
+						#if DEBUG
+							Serial.print("/switch: ");
+							Serial.print(json);
+						#endif
+
+						position == 1 ? switchOn(pin) : switchOff(pin);
 					}
 					else {
-						stopDehu();
-						strcpy(json, "{\"dehu\":false}");
+						strcpy(json, "{\"error\":\"Invalid channel\"}");
 					}
 				}
-				// /fan/?     1 = on, else off
-				else if (strncmp(resource, "fan", 4) == 0) {
-					#if DEBUG
-						Serial.print("Fan: ");
-						Serial.println(param1);
-					#endif
-					if(strncmp(param1, "1", 1) == 0) {
-						startFan();
-						strcpy(json, "{\"fan\":true}");
-					}
-					else {
-						stopFan();
-						strcpy(json, "{\"fan\":false}");
-					}
-				}
+
 				// /reboot
 				else if (strncmp(resource, "reboot", 6) == 0) {
 					#if DEBUG
@@ -422,7 +490,11 @@ void handleWebRequest() {
 					strcpy(json, "{\"reboot\":true}");
 					reset = true;
 				}
+
 				else {
+					#if DEBUG
+						Serial.println("404 NOT FOUND");
+					#endif
 					send404();
 					break;
 				}
@@ -463,4 +535,10 @@ void send404() {
 	httpClient.println(string_buffer);
 
 	httpClient.println();
+}
+
+int availableMemory() {
+  int free_memory;
+  return ((int) __brkval == 0) ? ((int) &free_memory) - ((int) &__bss_end) :
+      ((int) &free_memory) - ((int) __brkval);
 }
